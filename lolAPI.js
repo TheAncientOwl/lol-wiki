ff805d005f5e927a4138b252e1237d03cd973b7b
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-14 20:18:12 +0200

    added min-card route.

--- lolAPI.js
+++ lolAPI.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const fs = require('fs');
 
-router.get('/:champion', (req, res) => {
-  console.log(`Requested data for ${req.params.champion}`);
-  res.json({ ok: 200 });
+const IMAGES_LINK = 'http://localhost:5000/images';
+
+const getChampionData = name => {
+  const dataPath = path.join(__dirname, 'assets', 'data', 'en_US', 'champion', name + '.json');
+  const rawData = fs.readFileSync(dataPath);
+  const data = JSON.parse(rawData).data[name];
+
+  return data;
+};
+
+router.get('/:champion/min-card', async (req, res) => {
+  try {
+    const championData = getChampionData(req.params.champion);
+
+    res.json({
+      id: championData.key,
+      avatarURL: `${IMAGES_LINK}/champion/avatar/${championData.image.full}`,
+      name: championData.name,
+      tags: championData.tags,
+      blurb: championData.blurb,
+    });
+  } catch (err) {
+    res.status(404).json({ message: 'Unknown champion' });
+  }
 });
 
 module.exports = router;
