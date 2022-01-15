8dcf9c317cab2ddca7b7bfa7b6731edd0a71f7f9
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-15 13:49:06 +0200

    styled blank page.

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,12 @@ export const App = () => {
   }, []);
 
   return (
-    <div>
-      <img src='http://localhost:5000/images/champion/skin/Akshan_1.jpg' alt='akshan skin' />
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2em' }}>
+      <img
+        style={{ width: '700px', height: 'auto' }}
+        src='http://localhost:5000/images/champion/skin/Akshan_1.jpg'
+        alt='akshan skin'
+      />
     </div>
   );
 };
