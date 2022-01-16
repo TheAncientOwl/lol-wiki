ef0de3c40f7a38a198968ae20b89896602cf0a5b
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-16 02:54:46 +0200

    removed fragment.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,11 +3,9 @@ import LogoSrc from '../images/logo.png';
 
 export const Navbar = () => {
   return (
-    <>
-      <div className='lol-navbar text-white'>
-        <img src={LogoSrc} alt='LOL' className='lol-logo' />
-        <h5 className='lol-title'>League of Legends WIKI</h5>
-      </div>
-    </>
+    <div className='lol-navbar text-white'>
+      <img src={LogoSrc} alt='LOL' className='lol-logo' />
+      <h5 className='lol-title'>League of Legends WIKI</h5>
+    </div>
   );
 };
