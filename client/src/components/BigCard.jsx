47c345118de27d47579ec5d63bf9061fb262da58
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-16 17:18:26 +0200

    added start for components.

--- client/src/components/BigCard.jsx
+++ client/src/components/BigCard.jsx
@@ -1,18 +1,25 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { BigCardProfile } from './BigCardProfile';
 
 export const BigCard = ({ championName }) => {
+  const [profile, setProfile] = useState({});
   const [lore, setLore] = useState({});
   const [spells, setSpells] = useState({});
   const [skins, setSkins] = useState([]);
   const [overview, setOverview] = useState({});
 
   useEffect(() => {
+    axios.get(`/api/${championName}/profile`).then(response => setProfile(response.data));
     axios.get(`/api/${championName}/lore`).then(response => setLore(response.data));
     axios.get(`/api/${championName}/spells`).then(response => setSpells(response.data));
     axios.get(`/api/${championName}/skins`).then(response => setSkins(response.data));
     axios.get(`/api/${championName}/overview`).then(response => setOverview(response.data));
   }, [championName]);
 
-  return <div>{championName}</div>;
+  return (
+    <div>
+      <BigCardProfile avatarURL={profile.avatarURL} name={profile.name} title={profile.title} />
+    </div>
+  );
 };
---
+++ client/src/components/BigCardProfile.jsx
@@ -0,0 +1,3 @@
+export const BigCardProfile = ({ avatarURL, name, title }) => {
+  return <div>{name}</div>;
+};
