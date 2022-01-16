3ed1e88e9807dbb76b4cbab995985759943adeed
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-16 02:54:21 +0200

    added 2 empty lines between components.

--- client/src/components/SmallCard.jsx
+++ client/src/components/SmallCard.jsx
@@ -4,10 +4,12 @@ export const SmallCard = ({ id, number, avatarURL, name, tags, blurb }) => {
   return (
     <div className='card small-card'>
       <div className='small-card-number'>{number}</div>
+
       <div style={{ position: 'relative' }}>
         <img src={avatarURL} alt={name} className='card-img-top small-card-img' />
         <h6 className='small-card-read-more'>read more</h6>
       </div>
+
       <div className='card-body small-card-body'>
         <h6 className='card-title small-card-title'>{name}</h6>
         <p className='card-text small-card-blurb'>
