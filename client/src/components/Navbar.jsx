44d25660bc58ac13c9984ae1bb200b0f719f415d
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-16 18:39:28 +0200

    made navbar responsive.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,25 +7,29 @@ export const Navbar = ({ onSearch, onReset }) => {
 
   return (
     <div className='lol-navbar text-white'>
-      <img src={LogoSrc} alt='LOL' className='lol-logo' />
-      <h5 className='lol-title' onClick={onReset}>
-        League of Legends WIKI
-      </h5>
-
-      <div className='input-group input-group-sm navbar-search-field'>
-        <input
-          type='text'
-          className='form-control'
-          placeholder='Champion'
-          aria-label='Champion'
-          aria-describedby='basic-addon1'
-          onChange={e => setSearchFilter(e.target.value)}
-        />
+      <div className='navbar-home'>
+        <img src={LogoSrc} alt='LOL' className='lol-logo' />
+        <h5 className='lol-title' onClick={onReset}>
+          League of Legends WIKI
+        </h5>
       </div>
 
-      <button className='btn btn-sm btn-dark-gold' onClick={() => onSearch(searchFilter)}>
-        <i className='bi bi-search'></i>
-      </button>
+      <div className='d-flex navbar-search-field'>
+        <div className='input-group input-group-sm'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Champion'
+            aria-label='Champion'
+            aria-describedby='basic-addon1'
+            onChange={e => setSearchFilter(e.target.value)}
+          />
+        </div>
+
+        <button className='btn btn-sm btn-dark-gold' onClick={() => onSearch(searchFilter)}>
+          <i className='bi bi-search'></i>
+        </button>
+      </div>
     </div>
   );
 };
