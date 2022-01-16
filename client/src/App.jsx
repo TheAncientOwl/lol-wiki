b8a870a186b3df1f4847d9a4d6d38c56f7286001
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-16 02:47:15 +0200

    added small cards.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Navbar } from './components/Navbar';
 import './scss/App.scss';
 import axios from 'axios';
-import { MinCard } from './components/MinCard';
+import { SmallCard } from './components/SmallCard';
 
 export const App = () => {
   const [page, setPage] = useState(1);
@@ -20,7 +20,7 @@ export const App = () => {
         <div className='row g-1'>
           {champions.map(champion => (
             <div key={champion.id} className='col-12 col-md-6 col-lg-3 d-flex align-items-stretch'>
-              <MinCard
+              <SmallCard
                 number={champion.number}
                 id={champion.id}
                 avatarURL={champion.avatarURL}
---
+++ client/src/components/SmallCard.jsx
@@ -0,0 +1,20 @@
+import '../scss/SmallCard.scss';
+
+export const SmallCard = ({ id, number, avatarURL, name, tags, blurb }) => {
+  return (
+    <div className='card small-card'>
+      <div className='small-card-number'>{number}</div>
+      <div style={{ position: 'relative' }}>
+        <img src={avatarURL} alt={name} className='card-img-top small-card-img' />
+        <h6 className='small-card-read-more'>read more</h6>
+      </div>
+      <div className='card-body small-card-body'>
+        <h6 className='card-title small-card-title'>{name}</h6>
+        <p className='card-text small-card-blurb'>
+          <span className='small-card-tab' />
+          {blurb}
+        </p>
+      </div>
+    </div>
+  );
+};
