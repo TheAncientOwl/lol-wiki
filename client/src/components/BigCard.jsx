eef474a11bf31218d64e5045625c81e0cb999e35
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-17 14:07:13 +0200

    changed to promise.all

--- client/src/components/BigCard.jsx
+++ client/src/components/BigCard.jsx
@@ -6,25 +6,32 @@ import { BigCardProfile } from './BigCardProfile';
 import { BigCardSpells } from './BigCardSpells';
 
 export const BigCard = ({ championName }) => {
-  const [profile, setProfile] = useState({});
-  const [lore, setLore] = useState({});
-  const [spells, setSpells] = useState({});
-  const [skins, setSkins] = useState([]);
-  const [overview, setOverview] = useState({});
+  const [state, setState] = useState({ profile: {}, lore: {}, spells: {}, skins: [], overview: {} });
+  const { profile, lore, spells, skins, overview } = state;
 
   useEffect(() => {
-    axios.get(`/api/${championName}/profile`).then(response => setProfile(response.data));
-    axios.get(`/api/${championName}/lore`).then(response => setLore(response.data));
-    axios.get(`/api/${championName}/spells`).then(response => setSpells(response.data));
-    axios.get(`/api/${championName}/skins`).then(response => setSkins(response.data));
-    axios.get(`/api/${championName}/overview`).then(response => setOverview(response.data));
+    Promise.all([
+      axios.get(`/api/${championName}/profile`),
+      axios.get(`/api/${championName}/lore`),
+      axios.get(`/api/${championName}/spells`),
+      axios.get(`/api/${championName}/skins`),
+      axios.get(`/api/${championName}/overview`),
+    ]).then(values => {
+      setState({
+        profile: values[0].data,
+        lore: values[1].data,
+        spells: values[2].data,
+        skins: values[3].data,
+        overview: values[4].data,
+      });
+    });
   }, [championName]);
 
   return (
     <div className='card big-card'>
       <BigCardProfile avatarURL={profile.avatarURL} name={profile.name} title={profile.title} />
       <BigCardLore lore={lore.lore} />
-      <BigCardSpells q={spells.q} w={spells.w} e={spells.e} passive={spells.passive} />
+      {/* <BigCardSpells q={spells.q} w={spells.w} e={spells.e} passive={spells.passive} /> */}
     </div>
   );
 };
