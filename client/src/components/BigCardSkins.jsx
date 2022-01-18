714f4597c2e16cc5577283ea53679ae73d1ec2c5
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-19 01:18:58 +0200

    now display skin's name

--- client/src/components/BigCardSkins.jsx
+++ client/src/components/BigCardSkins.jsx
@@ -1,17 +1,28 @@
+import { useCallback, useState } from 'react';
 import ImageGallery from 'react-image-gallery';
 
 export const BigCardSkins = ({ skins, championName }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const images = skins.map(skin => ({ original: skin.imageURL, thumbnail: skin.imageURL }));
+
+  const handleIndexChange = useCallback(index => setActiveIndex(index), [setActiveIndex]);
 
   return (
     <section className='big-card-section big-card-skins-container'>
       <h5 className='big-card-section-title'>Skins</h5>
 
       <div className='big-card-skins-gallery'>
-        <ImageGallery items={images} showFullscreenButton={false} showPlayButton={false} />
+        <ImageGallery
+          onBeforeSlide={handleIndexChange}
+          items={images}
+          showFullscreenButton={false}
+          showPlayButton={false}
+        />
         <div className='big-card-skins-info'>
           <span className='big-card-skins-separator'></span>
-          <h6 className='badge big-card-skins-skin-title'>Akshan</h6>
+          <h6 className='badge big-card-skins-skin-title'>
+            {skins[activeIndex]?.name === 'default' ? championName : skins[activeIndex]?.name}
+          </h6>
           <span className='big-card-skins-separator'></span>
         </div>
       </div>
