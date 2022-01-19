4154d68f0835bb853ea1553734ccb0fa00d927ec
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-19 07:18:46 +0200

    added text-tab before lore

--- client/src/components/BigCardLore.jsx
+++ client/src/components/BigCardLore.jsx
@@ -1,7 +1,10 @@
 export const BigCardLore = ({ lore }) => {
   return (
     <section className='big-card-section'>
-      <div className='big-card-lore text-dark-gray'>{lore}</div>
+      <div className='big-card-lore text-dark-gray'>
+        <span className='text-tab' />
+        {lore}
+      </div>
     </section>
   );
 };
