cd42e015ba1811d02b6278be1eac2de2ffbc52cb
TheAncientOwl <dele.alex.o.o@gmail.com>
2022-01-21 10:09:10 +0200

    ??

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use('/images', express.static(path.join(__dirname, 'assets/img')));
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
